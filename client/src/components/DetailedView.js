3098a971dae6d7666a914fef4007ea70a9db9684
Adel Wu <adelwu@seas.upenn.edu>
2020-04-24 23:09:17 -0500

    detail view additions, FactCard layout

--- client/src/components/DetailedView.js
+++ client/src/components/DetailedView.js
@@ -1,16 +1,160 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FactCard from "./FactCard";
+import testData from "../testData";
 
 export default class DetailedView extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
 
-	render() {
-		return (
-			<div className="detailedView" id={this.props.id} onClick={this.props.onClick}>
-        <p style={{color:'white'}}>I am DetailedView Component</p>
-			</div>
-		);
-	}
+    this.state = {
+      mediaId: "",
+      mediaType: "M",
+      keywords: [],
+      avg_rating: 0,
+      img_url: "",
+      recMedia: [],
+    };
+
+    this.getMediaInfo = this.getMediaInfo.bind(this);
+    this.submitMedia = this.submitMedia.bind(this);
+  }
+
+  //TODO: set mediaId passed from dashboard
+
+  //TODO: get the selected media's information
+  getMediaInfo() {
+    fetch("http://localhost:8081/media/" + this.state.mediaId, {
+      method: "GET", // The type of HTTP request.
+    })
+      .then(
+        (res) => {
+          // Convert the response data to a JSON.
+          return res.json();
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      )
+      .then(
+        (mediaInfo) => {
+          if (!mediaInfo) return;
+
+          //TODO: set states using response json
+          this.setState({
+            mediaType: mediaInfo.media_type,
+            keywords: mediaInfo.keywords,
+            avg_rating: mediaInfo.avg_rating,
+            img_url: mediaInfo.img_url,
+          });
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      );
+  }
+
+  //TODO: get a list of recommendations + create FactCards
+  submitMedia() {
+    // Send an HTTP request to the server.
+    fetch("http://localhost:8081/recommendations/" + this.state.mediaId, {
+      method: "GET", // The type of HTTP request.
+    })
+      .then(
+        (res) => {
+          // Convert the response data to a JSON.
+          return res.json();
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      )
+      .then(
+        (recList) => {
+          if (!recList) return;
+          let recDivs = recList.map((rec, i) => (
+            //TODO: Pass attributes here to FactCard
+            <FactCard
+              id={0}
+              genre={0}
+              title={0}
+              avg_rating={0}
+              rating_count={0}
+              desc={0}
+            />
+          ));
+
+          this.setState({
+            recMedia: recDivs,
+          });
+        },
+        (err) => {
+          // Print the error if there is one.
+          console.log(err);
+        }
+      );
+  }
+
+  render() {
+    //MOVIE
+    if (this.state.mediaType == "M") {
+      return (
+        <div
+          className="detailedView"
+          id={this.props.id}
+          onClick={this.props.onClick}
+        >
+          <section class="jumbotron">
+            <img
+              src="http://image.tmdb.org/t/p/w185//vzmL6fP7aPKNKPRTFnZmiUfciyV.jpg"
+              class="float-left align-middle"
+              alt="Movie Image"
+              height="250"
+              style={{ marginRight: "30px" }}
+            ></img>
+            <div class="container">
+              <h1 class="jumbotron-heading">Movie Title</h1>
+              <p class="lead text-muted">Genres: </p>
+              <p class="lead text-muted">Avg. Rating: __ (Number of Ratings)</p>
+              <p class="lead text-muted">Keywords: </p>
+              <p class="lead text-muted">Runtime:</p>
+            </div>
+          </section>
+
+          <h2 style={{ color: "white" }}>Recommendations you might enjoy</h2>
+        </div>
+      );
+    }
+
+    //BOOK
+    return (
+      <div
+        className="detailedView"
+        id={this.props.id}
+        onClick={this.props.onClick}
+      >
+        <section class="jumbotron">
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/61VUik8NJ8L.jpg"
+            class="float-left align-middle"
+            alt="Movie Image"
+            height="250"
+            style={{ marginRight: "30px" }}
+          ></img>
+          <div class="container">
+            <h1 class="jumbotron-heading">Movie Title</h1>
+            <p class="lead text-muted">Genres: </p>
+            <p class="lead text-muted">Avg. Rating: __ (Number of Ratings)</p>
+            <p class="lead text-muted">Keywords: </p>
+            <p class="lead text-muted">Runtime:</p>
+          </div>
+        </section>
+
+        <h2 style={{ color: "white" }}>Recommendations you might enjoy</h2>
+      </div>
+    );
+  }
 }
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
-var config = require('./db-config.js');
-var mysql = require('mysql');
+var config = require("./db-config.js");
+var mysql = require("mysql");
 
 config.connectionLimit = 10;
 var connection = mysql.createPool(config);
@@ -8,86 +8,16 @@ var connection = mysql.createPool(config);
 /* ------------------- Route Handlers --------------- */
 /* -------------------------------------------------- */
 
-
-/* ---- Q1a (Dashboard) ---- */
-function getAllGenres(req, res) {
+// TODO: Getting Media Info
+// Given an ID, find the media item that matches - for details page
+function getMediaInfo(req, res) {
   let query = `
-    SELECT DISTINCT genre
-    FROM Genres;
+    SELECT *
+    FROM Media
+    WHERE media_id = ${req.params.id};
   `;
 
-  connection.query(query, function(err, rows, fields) {
-    if (err) console.log(err);
-    else {
-      res.json(rows);
-    }
-  });
-};
-
-
-/* ---- Q1b (Dashboard) ---- */
-function getTopInGenre(req, res) {
-  let query = `
-    SELECT title, rating, vote_count 
-    FROM Genres JOIN Movies
-    ON Genres.movie_id = Movies.id
-    WHERE Genres.genre = '${req.params.genre}'
-    ORDER BY rating DESC, vote_count DESC 
-    LIMIT 10; 
-  `;
-
-  connection.query(query, function(err, rows, fields) {
-    if (err) console.log(err);
-    else {
-      res.json(rows);
-    }
-  });
-};
-
-/* ---- Q2 (Recommendations) ---- */
-function getRecs(req, res) {
-  let query = `
-    WITH matching_genres AS (SELECT genre
-    FROM Genres JOIN Movies
-    ON Genres.movie_id = Movies.id
-    WHERE Movies.title = '${req.params.title}')
-    
-    SELECT title, id, rating, vote_count 
-    FROM Movies JOIN (
-    SELECT movie_id, COUNT(*)
-    FROM Genres 
-    WHERE Genres.genre IN (SELECT genre FROM matching_genres)
-    GROUP BY movie_id
-    HAVING COUNT(*) >= ALL (
-    SELECT COUNT(*) FROM (
-    SELECT genre FROM matching_genres) C
-    ) 
-    ) A
-    ON Movies.id = A.movie_id
-    WHERE Movies.title <> '${req.params.title}'
-    ORDER BY rating DESC, vote_count DESC
-    LIMIT 5;
-  `;
-
-  connection.query(query, function(err, rows, fields) {
-    if (err) console.log(err);
-    else {
-      res.json(rows);
-    }
-  });
-};
-
-/* ---- (Best Genres) ---- */
-function getDecades(req, res) {
-	var query = `
-    SELECT DISTINCT (FLOOR(year/10)*10) AS decade
-    FROM (
-      SELECT DISTINCT release_year as year
-      FROM Movies
-      ORDER BY release_year
-    ) y
-  `;
-  connection.query(query, function(err, rows, fields) {
+  connection.query(query, function (err, rows, fields) {
     if (err) console.log(err);
     else {
       res.json(rows);
@@ -95,36 +25,28 @@ function getDecades(req, res) {
   });
 }
 
-/* ---- Q3 (Best Genres) ---- */
-function bestGenresPerDecade(req, res) {
-  var query = `
-    WITH distinct_genres AS (SELECT DISTINCT genre FROM Genres)
-
-    SELECT distinct_genres.genre, IFNULL(avg_rating, 0) AS avg_rating, IFNULL(decade, ${req.params.decade}) AS decade
-    FROM distinct_genres LEFT JOIN (
-      SELECT floor(release_year / 10) * 10 AS decade, genre, AVG(rating) AS avg_rating
-      FROM(
-        SELECT release_year, genre, rating
-        FROM Movies JOIN Genres
-        ON Movies.id = Genres.movie_id) A
-      WHERE floor(release_year / 10) * 10 = ${req.params.decade}
-      GROUP BY floor(release_year / 10) * 10, genre) decade_genre_rating
-    ON distinct_genres.genre = decade_genre_rating.genre
-    ORDER BY avg_rating DESC, genre;
+// TODO: Getting Media's Recommendations
+// Given an ID, output a list of media items - for details page
+function getRecs(req, res) {
+  let query = `
+    SELECT *
+    FROM Media
+    WHERE media_id = ${req.params.id};
   `;
-  connection.query(query, function(err, rows, fields) {
+
+  connection.query(query, function (err, rows, fields) {
     if (err) console.log(err);
     else {
       res.json(rows);
     }
   });
-};
+}
 
 // The exported functions, which can be accessed in index.js.
 module.exports = {
-	getAllGenres: getAllGenres,
-	getTopInGenre: getTopInGenre,
-	getRecs: getRecs,
-	getDecades: getDecades,
-  bestGenresPerDecade: bestGenresPerDecade
-}
+  getAllGenres: getAllGenres,
+  getTopInGenre: getTopInGenre,
+  getRecs: getRecs,
+  getDecades: getDecades,
+  bestGenresPerDecade: bestGenresPerDecade,
+};
