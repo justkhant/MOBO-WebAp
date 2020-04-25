6b3024dd0dc0ab3ff31c806fec5e902d146b17c1
Adel Wu <adelwu@seas.upenn.edu>
2020-04-25 15:24:57 -0500

    adjustments to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import '../style/Dashboard.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import "../style/Dashboard.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
-import SearchBar from './SearchBar';
-import SearchResults from './SearchResults';
-import DetailedView from './DetailedView';
-import FactCard from './FactCard';
+import SearchBar from "./SearchBar";
+import SearchResults from "./SearchResults";
+import DetailedView from "./DetailedView";
+import FactCard from "./FactCard";
 
-import testData from '../testData';
+import testData from "../testData";
 
 export default class Dashboard extends React.Component {
   constructor(props) {
@@ -16,10 +16,10 @@ export default class Dashboard extends React.Component {
 
     this.state = {
       currentSearchTerm: null,
-      searchResultsData: null,
-      detailedViewData: null, 
+      searchResultsData: {},
+      detailedViewData: null,
       isDetailedView: false,
-    }
+    };
 
     this.search = this.search.bind(this);
     this.showDetailedView = this.showDetailedView.bind(this);
@@ -29,37 +29,57 @@ export default class Dashboard extends React.Component {
   // React function that is called when the page load.
   componentDidMount() {
     // TODO: Fetch data for interesting facts section
-    
   }
 
   search(searchData) {
     // TODO: Fetch data for search term, onClick/onSubmit for SearchBar
-    var searchPath = [searchData.media, searchData.genre, searchData.searchTerm].join("/")
-    console.log(searchPath)
+    var searchPath = [
+      searchData.media,
+      searchData.genre,
+      searchData.searchTerm,
+    ].join("/");
+    console.log(searchPath);
 
     fetch("http://localhost:8081/search/" + searchPath, {
-      method:"GET"
-    }).then(res=> {
-      console.log("searchresult" )
-      console.log(res.json())
+      method: "GET",
     })
+      .then(
+        (res) => {
+          // Convert the response data to a JSON.
+          return res.json();
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
+      .then(
+        (searchResult) => {
+          if (!searchResult) return;
+          console.log(searchResult);
 
-
+          this.setState({
+            searchResultsData: searchResult,
+          });
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
   }
 
   showDetailedView(rowIndex) {
     console.log(rowIndex);
-    
+
     this.setState({
-      detailedViewData: testData, 
+      detailedViewData: testData,
       isDetailedView: true,
-    })
+    });
   }
 
   hideDetailedView() {
     this.setState({
       isDetailedView: false,
-    })
+    });
   }
 
   render() {
@@ -68,10 +88,12 @@ export default class Dashboard extends React.Component {
     if (isDetailedView) {
       return (
         <div className="Dashboard">
-          <h1 style={{color:'white'}}>Is DetailedView</h1>
+          <h1 style={{ color: "white" }}>Is DetailedView</h1>
           <br></br>
-          <SearchBar search={this.search}/>
-          <Button variant="primary" onClick={this.hideDetailedView}>Hide Detailed View</Button>{' '}
+          <SearchBar search={this.search} />
+          <Button variant="primary" onClick={this.hideDetailedView}>
+            Hide Detailed View
+          </Button>{" "}
           <DetailedView data={detailedViewData[0]} />
         </div>
       );
@@ -79,11 +101,16 @@ export default class Dashboard extends React.Component {
 
     return (
       <div className="Dashboard">
-        <h1 style={{color:'white'}}>Not DetailedView</h1>
+        <h1 style={{ color: "white" }}>Not DetailedView</h1>
         <br></br>
-        <SearchBar search={this.search}/>
-        <Button variant="primary" onClick={this.showDetailedView}>Show Detailed View</Button>{' '}
-        <SearchResults data={testData} showDetailedView={this.showDetailedView}/>
+        <SearchBar search={this.search} />
+        <Button variant="primary" onClick={this.showDetailedView}>
+          Show Detailed View
+        </Button>{" "}
+        <SearchResults
+          data={testData}
+          showDetailedView={this.showDetailedView}
+        />
       </div>
     );
   }
--- server/routes.js
+++ server/routes.js
@@ -72,7 +72,7 @@ function titleSearch(req, res) {
         WHERE (UTL_MATCH.edit_distance_similarity(query, LOWER(title)) > 80 OR (LOWER(title) LIKE CONCAT(CONCAT('%', query), '%')))
         ORDER BY similarity DESC
         )
-    WHERE ROWNUM <= 10;
+    WHERE ROWNUM <= 10
     
     `;
       res.json(run(query));
