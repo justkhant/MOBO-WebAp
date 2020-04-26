fce59f5f368968a438b33f9cc06264e332bb6089
Bill Lou <billloukehao@gmail.com>
2020-04-25 22:03:37 -0700

    Fix syntax with className instead of class

--- client/src/components/DetailsView.js
+++ client/src/components/DetailsView.js
@@ -12,43 +12,43 @@ export default class DetailsView extends React.Component {
     // BOOK
     if (data[4] === "B") {
       return (
-        <section class="jumbotron">
+        <section className="jumbotron">
           <img
             src="https://images-na.ssl-images-amazon.com/images/I/61VUik8NJ8L.jpg"
-            class="float-left align-middle"
+            className="float-left align-middle"
             alt="Movie Image"
             height="250"
             style={{ marginRight: "30px" }}
           ></img>
-          <div class="container">
-            <h1 class="jumbotron-heading">{data[1]}</h1>
-            <p class="lead text-muted">Authors: {data[9]}</p>
-            <p class="lead text-muted">
+          <div className="container">
+            <h1 className="jumbotron-heading">{data[1]}</h1>
+            <p className="lead text-muted">Authors: {data[9]}</p>
+            <p className="lead text-muted">
               Avg. Rating: {data[6]} ({data[11]})
             </p>
-            <p class="lead text-muted">Review Count: {data[13]}</p>
-            <p class="lead text-muted">Keywords: {data[5]}</p>
-            <p class="lead text-muted">Page Count: {data[12]}</p>
+            <p className="lead text-muted">Review Count: {data[13]}</p>
+            <p className="lead text-muted">Keywords: {data[5]}</p>
+            <p className="lead text-muted">Page Count: {data[12]}</p>
           </div>
         </section>
       );
     }
     //MOVIE
     return (
-      <section class="jumbotron">
+      <section className="jumbotron">
         <img
           src="http://image.tmdb.org/t/p/w185//vzmL6fP7aPKNKPRTFnZmiUfciyV.jpg"
-          class="float-left align-middle"
+          className="float-left align-middle"
           alt="Movie Image"
           height="250"
           style={{ marginRight: "30px" }}
         ></img>
-        <div class="container">
-          <h1 class="jumbotron-heading">Movie Title</h1>
-          <p class="lead text-muted">Genres: </p>
-          <p class="lead text-muted">Avg. Rating: __ (Number of Ratings)</p>
-          <p class="lead text-muted">Keywords: </p>
-          <p class="lead text-muted">Runtime:</p>
+        <div className="container">
+          <h1 className="jumbotron-heading">Movie Title</h1>
+          <p className="lead text-muted">Genres: </p>
+          <p className="lead text-muted">Avg. Rating: __ (Number of Ratings)</p>
+          <p className="lead text-muted">Keywords: </p>
+          <p className="lead text-muted">Runtime:</p>
         </div>
       </section>
     );
