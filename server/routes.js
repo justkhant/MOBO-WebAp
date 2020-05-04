f5f805d03be94f4473506aaf1f94ce18343fbec9
justkhant <khant.kky@gmail.com>
2020-05-04 15:41:38 -0400

    fixed recs

--- server/routes.js
+++ server/routes.js
@@ -64,7 +64,7 @@ function getMediaInfo(req, res) {
            ELSE B.rating_count END) AS rating_count, 
         revenue, runtime, language, authors, pages, review_count
   FROM Media M LEFT OUTER JOIN Movies Mo ON M.media_id =  Mo.media_id LEFT OUTER JOIN Books B ON M.media_id = B.media_id
-  WHERE media_id = ${req.params.id}
+  WHERE M.media_id = ${req.params.id}
   `;
 
   run(query).then((response) => {
@@ -91,7 +91,7 @@ function getMultipleMediaInfo(req, res) {
     `;
   }
   media_ids.forEach(function(media_id) {
-      query.concat(` media_id = `+ media_id +` OR`);
+      query.concat(` M.media_id = `+ media_id +` OR`);
   } );
 
   query.substring(0, query.length - 2);
@@ -141,44 +141,50 @@ function titleSearch(req, res) {
 function getAllGenres(req, res) {}
 
 function getRecs(req, res) {
-  var searchTitle = req.params.searchTitle;
+  var searchId = req.params.searchId;
+  var searchType = req.params.searchType;
 
   var query =
-    `WITH input_keywords AS (
+
+  `WITH input_keywords AS (
       SELECT DISTINCT media_id, title, trim(regexp_substr(Media.keywords, '[^,]+', 1, levels.column_value)) AS keyword
       FROM 
       Media, 
       table(cast(multiset(select level from dual connect by  level <= length (regexp_replace(Media.keywords, '[^,]+'))  + 1) as sys.OdciNumberList)) levels
-      WHERE title = '`+searchTitle+`' AND trim(regexp_substr(Media.keywords, '[^,]+', 1, levels.column_value)) IS NOT NULL
-     ),
-    all_keywords AS (
+      WHERE media_id = `+ searchId +` AND trim(regexp_substr(Media.keywords, '[^,]+', 1, levels.column_value)) IS NOT NULL
+  ),
+  all_keywords AS (
       SELECT DISTINCT media_id, title, trim(regexp_substr(Media.keywords, '[^,]+', 1, levels.column_value)) AS keyword
       FROM 
       Media, 
       table(cast(multiset(select level from dual connect by  level <= length (regexp_replace(Media.keywords, '[^,]+'))  + 1) as sys.OdciNumberList)) levels
-      WHERE title <> '`+searchTitle+`' AND trim(regexp_substr(Media.keywords, '[^,]+', 1, levels.column_value)) IS NOT NULL
+      WHERE media_id <> `+ searchId +` AND trim(regexp_substr(Media.keywords, '[^,]+', 1, levels.column_value)) IS NOT NULL
   ), 
   keyword_match AS (
       SELECT media_id, title, SUM(score) AS score
       FROM (
-          SELECT a.media_id, a.title, i.keyword, a.keyword, (10) AS score 
+          SELECT a.media_id, a.title, i.keyword, a.keyword, (30) AS score 
           From input_keywords i, all_keywords a
           WHERE i.keyword like a.keyword
           )
       GROUP BY media_id, title
   ),
   
-  input_genre AS (
-    SELECT M.media_id, M.title, G.genre_name
-    FROM Media M JOIN Genres_to_media G ON M.media_id = G.media_id
-    WHERE M.title = '`+searchTitle+`' 
+  input_genre_similarity_id AS (
+    SELECT DISTINCT M.media_id, M.title, Ge.similarity_id 
+    FROM Media M JOIN Genres_to_media G ON M.media_id = G.media_id JOIN Genres Ge ON G.genre_name = Ge.genre_name
+    WHERE M.media_id = `+ searchId +`
   ),
+  
   genre_match AS (
     SELECT M.media_id, M.title, (COUNT(*) * 5) AS score
-    FROM input_genre I JOIN Genres_to_media G ON I.genre_name = G.genre_name JOIN Media M ON M.media_id = G.media_id
-      WHERE M.title <> '`+searchTitle+`'
+    FROM input_genre_similarity_id I JOIN Genres Ge ON I.similarity_id = Ge.similarity_id 
+                                          JOIN Genres_to_media G ON Ge.genre_name = G.genre_name 
+                                              JOIN Media M ON M.media_id = G.media_id 
+      WHERE M.media_id <> `+ searchId +`
     GROUP BY M.media_id, M.title
   ),
+  
   movie_info AS (
       SELECT Me.media_id, title, release_date
       FROM Media Me JOIN Movies M ON Me.media_id = M.media_id
@@ -187,27 +193,66 @@ function getRecs(req, res) {
   input_decade AS (
       SELECT TO_NUMBER(EXTRACT(YEAR FROM release_date), '9999') - MOD((TO_NUMBER(EXTRACT(YEAR FROM release_date), '9999')), 10) AS decade
       FROM movie_info
-      WHERE title = '`+searchTitle+`'
+      WHERE media_id = `+ searchId +`
   ),
+  
   decade_match AS (
-      SELECT media_id, title, 2 AS score
+      SELECT media_id, title, 5 AS score
       FROM movie_info, input_decade 
       WHERE decade = TO_NUMBER(EXTRACT(YEAR FROM release_date), '9999') - MOD((TO_NUMBER(EXTRACT(YEAR FROM release_date), '9999')), 10) 
-          AND title <> '`+searchTitle+`'
+          AND media_id <> `+ searchId +`
+  ),
+  
+  book_info AS (
+      SELECT Me.media_id, title, B.authors
+      FROM Media Me JOIN Books B ON Me.media_id = B.media_id
+  ),
+  
+  input_authors AS (
+      SELECT DISTINCT media_id, title, trim(regexp_substr(authors, '[^|]+', 1, levels.column_value)) AS author
+      FROM book_info, table(cast(multiset(select level from dual connect by  level <= length (regexp_replace(authors, '[^|]+'))  + 1) as sys.OdciNumberList)) levels
+      WHERE media_id = `+ searchId +` AND trim(regexp_substr(authors, '[^|]+', 1, levels.column_value)) IS NOT NULL
+  ),
+  all_authors AS (
+      SELECT DISTINCT media_id, title, trim(regexp_substr(authors, '[^|]+', 1, levels.column_value)) AS author
+      FROM book_info, table(cast(multiset(select level from dual connect by  level <= length (regexp_replace(authors, '[^|]+'))  + 1) as sys.OdciNumberList)) levels
+      WHERE media_id <> `+ searchId +` AND trim(regexp_substr(authors, '[^|]+', 1, levels.column_value)) IS NOT NULL
+  ),
+  authors_match AS (
+      SELECT media_id, title, SUM(score) AS score
+      FROM (
+          SELECT a.media_id, a.title, i.author, a.author, (30) AS score 
+          From input_authors i, all_authors a
+          WHERE i.author = a.author
+          )
+      GROUP BY media_id, title
   ),
   top_scores AS (
-      SELECT *
-      FROM (
-          SELECT media_id, title, SUM(score) AS match_score
-          FROM ((SELECT* FROM keyword_match) UNION ALL (SELECT * FROM decade_match) UNION ALL (SELECT * FROM genre_match))
-          GROUP BY media_id, title
-          ORDER BY match_score DESC
-          )
-      WHERE ROWNUM <= 10
+      SELECT media_id, title, SUM(score) AS match_score
+      FROM ((SELECT* FROM keyword_match) UNION ALL (SELECT * FROM decade_match) 
+                                          UNION ALL (SELECT * FROM genre_match) 
+                                          UNION ALL (SELECT * FROM authors_match))
+      GROUP BY media_id, title
+      ORDER BY match_score DESC
   )
   
-  SELECT t.media_id, t.title, media_type, keywords, avg_rating, image_url, overview, release_date, rating_count, revenue, runtime, language, match_score
-  FROM top_scores t INNER JOIN Media m ON t.media_id = m.media_id INNER JOIN Movies mo ON t.media_id = mo.media_id
+    SELECT *
+      FROM (
+          SELECT M.media_id, M.title, media_type, t.match_score, keywords, 
+              (CASE media_type 
+                  WHEN 'M' THEN Mo.overview 
+                  ELSE B.description END) AS overview,
+               avg_rating, image_url, release_date, 
+              (CASE media_type 
+                   WHEN 'M' THEN Mo.rating_count 
+                  ELSE B.rating_count END) AS rating_count, 
+              revenue, runtime, language, authors, pages, review_count
+          FROM top_scores t INNER JOIN Media M ON t.media_id = M.media_id 
+              LEFT OUTER JOIN Movies Mo ON t.media_id = Mo.media_id 
+              LEFT OUTER JOIN Books B ON t.media_id = B.media_id
+          WHERE media_type = '`+ searchType +`'
+      )
+   WHERE ROWNUM <= 10
   `;
 
   run(query).then((response) => {
